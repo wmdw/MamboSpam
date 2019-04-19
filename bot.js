ce4a0a52fc8e5e523143f55d689a92450fb3ac26
wmdw <49795721+wmdw@users.noreply.github.com>
2019-04-19 13:09:41 -0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("561735462109052948")
+setInterval(function() {
+channel.send(`Чукииии`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
